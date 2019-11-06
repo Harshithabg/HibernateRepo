943c7bc5696657129b019b2dba48eae9dff8e204
Harshithabg <harshithabg97@gmail.com>
2019-11-06 15:55:19 +0530

    updated

---
+++ NewMavenProject/src/main/java/com/lti/hib_ex/Hibernate_Proj/App.java
@@ -0,0 +1,25 @@
+package com.lti.hib_ex.Hibernate_Proj;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.lti.hib_ex.Hibernate_Proj.Employee;
+
+public class App {
+public static void main(String args[]){
+	Configuration configuration = new Configuration().configure();
+	StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+	SessionFactory factory = configuration.buildSessionFactory(builder.build());
+	Session session = factory.openSession();
+	Employee employee = new Employee();
+	employee.setEmpName("HARSHITHA");
+	employee.setBranch("bangalore");
+	session.beginTransaction();
+	session.save(employee);
+	session.getTransaction().commit();
+	
+}
+}
